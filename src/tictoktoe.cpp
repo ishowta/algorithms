305da886578e8a12b1e8748595eee47244318bdb
ishowta <ishowta@gmail.com>
2020-10-28 23:18:32 +0900

    format

--- src/tictoktoe.cpp
+++ src/tictoktoe.cpp
@@ -1,64 +1,51 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <array>
-#include <random>
 #include <bitset>
+#include <iostream>
 #include <optional>
+#include <random>
 #include <unordered_map>
+#include <vector>
 
 using std::cout, std::endl, std::optional, std::make_pair;
 
-struct PutEvalState
-{
+struct PutEvalState {
     int index;
     int count;
     int sum_award;
 };
 
-struct BoardState
-{
+struct BoardState {
     std::vector<PutEvalState> put_states;
 };
 
-enum Player
-{
-    TIK,
-    TOK
-};
+enum Player { TIK, TOK };
 
-struct Board
-{
+struct Board {
     std::bitset<9> tik;
     std::bitset<9> tok;
 
     Board() : tik(0), tok(0) {}
 
-    bool operator==(const Board &rhs) const
-    {
+    bool operator==(const Board &rhs) const {
         return tik == rhs.tik && tok == rhs.tok;
     }
 
-    std::optional<Player> check() const
-    {
+    std::optional<Player> check() const {
         auto check_ = [](std::bitset<9> b) -> bool {
-            auto win_list = {std::bitset<9>(0b111000000),
-                             std::bitset<9>(0b000111000),
-                             std::bitset<9>(0b000000111),
-                             std::bitset<9>(0b100010001),
-                             std::bitset<9>(0b001010100),
-                             std::bitset<9>(0b100100100),
-                             std::bitset<9>(0b010010010),
-                             std::bitset<9>(0b001001001)};
-            return std::any_of(begin(win_list), end(win_list), [&](std::bitset<9> x) { return (x & b) == x; });
+            auto win_list = {
+                std::bitset<9>(0b111000000), std::bitset<9>(0b000111000),
+                std::bitset<9>(0b000000111), std::bitset<9>(0b100010001),
+                std::bitset<9>(0b001010100), std::bitset<9>(0b100100100),
+                std::bitset<9>(0b010010010), std::bitset<9>(0b001001001)};
+            return std::any_of(begin(win_list), end(win_list),
+                               [&](std::bitset<9> x) { return (x & b) == x; });
         };
         return check_(tik) ? TIK : check_(tok) ? TOK : optional<Player>();
     }
 
-    void print() const
-    {
-        for (size_t i = 0; i < 9; i++)
-        {
+    void print() const {
+        for (size_t i = 0; i < 9; i++) {
             cout << (this->tik[i] ? "⚪" : this->tok[i] ? "❌" : "　");
             if ((i + 1) % 3 == 0)
                 cout << endl;
@@ -67,19 +54,16 @@ struct Board
     }
 };
 
-struct BoardStateHash
-{
+struct BoardStateHash {
     typedef std::size_t result_type;
 
-    std::size_t operator()(Board const &rhs) const
-    {
+    std::size_t operator()(Board const &rhs) const {
         std::hash<std::bitset<9>> hash_fn;
         return hash_fn(rhs.tik) ^ hash_fn(rhs.tok);
     }
 };
 
-int main()
-{
+int main() {
     std::random_device rnd;
     std::mt19937 mt(rnd());
 
@@ -87,8 +71,7 @@ int main()
     Player player = TIK;
     std::unordered_map<Board, BoardState, BoardStateHash> board_states;
 
-    while (true)
-    {
+    while (true) {
         // board_statesを探して、無かったら新しく作る
         auto &board_state = ([&]() -> BoardState & {
             auto res = board_states.find(board);
@@ -97,8 +80,7 @@ int main()
 
             BoardState newboard_state;
 
-            for (int i = 0; i < 9; i++)
-            {
+            for (int i = 0; i < 9; i++) {
                 if (board.tik[i] == 0 && board.tok[i] == 0)
                     newboard_state.put_states.push_back(PutEvalState{i, 0, 0});
             }
